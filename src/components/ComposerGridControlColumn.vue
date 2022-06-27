088e86b284411ffc8b291b4cfda0652ec1580f26
Steve Hiehn <stevehiehn@Steves-Mac-mini.hitronhub.home>
2022-06-27 15:33:14 -0700

    prompt for name when creating a section;

--- src/components/ComposerGridControlColumn.vue
+++ src/components/ComposerGridControlColumn.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col min-w-fit bg-gray-200 w-1/12 border-2 border-black rounded-lg mr-2 pt-2  px-2">
-    <button @click="addSection" class="flex mb-2 w-full justify-center hover:cursor-pointer">
+    <button @click="addSectionPrompt" class="flex mb-2 w-full justify-center hover:cursor-pointer">
       <img :src=imageUrls.plusIcon class="w-6 h-6">
     </button>
     <div @click="handleClick('hi')"
@@ -34,6 +34,8 @@ import ComposerControls from "./ComposerControls";
 import GridProcessor from "../processors/grid-processor";
 import {v4} from "uuid";
 import useEventsBus from "../events/eventBus";
+import ModalOpenPayload from "./ModalOpenPayload";
+import {watch} from "vue";
 
 export default {
   name: "ComposerGridControlColumn",
@@ -42,7 +44,7 @@ export default {
   },
   setup() {
     const store = inject('store')
-    const {emit} = useEventsBus()
+    const {bus, emit} = useEventsBus()
     const toast = inject('toast');
     const imageUrls = {
       drumIcon: store.state.staticUrl + "icons/eq-drum.png",
@@ -53,10 +55,36 @@ export default {
       plusIcon: store.state.staticUrl + "icons/plus.png",
     }
 
-    const addSection = () => {
-      new GridProcessor(store.state.grid).addSection(v4())
+    const addSection = (sectionName) => {
+      if (!sectionName) {
+        sectionName = v4()
+      }
+
+      new GridProcessor(store.state.grid).addSection(sectionName)
       emit('renderMixIfNeeded')
     }
+
+    const addSectionModalId = 'addSectionModalId'
+    const addSectionPrompt = () => {
+      const modalOpenPayload = new ModalOpenPayload(
+          addSectionModalId,
+          'Add Section',
+          'Provide a name for the section',
+          'Add',
+          'Cancel',
+          true,
+      )
+
+      emit('launchModal', modalOpenPayload)
+    }
+
+    watch(() => bus.value.get('modalResponse'), (modalResponsePayload) => {
+      if (modalResponsePayload[0] && modalResponsePayload[0].getInstanceId() === addSectionModalId) {
+        if (modalResponsePayload[0].getResponse()) {
+          addSection(modalResponsePayload[0].getResponse())
+        }
+      }
+    })
 
     const handleClick = (type) => {
       let updateParam = {
@@ -68,11 +96,12 @@ export default {
     }
 
     return {
-      addSection,
+      addSectionPrompt,
       handleClick,
       imageUrls
     }
-  },
+  }
+  ,
 }
 </script>
 
