5c3f8eb0d7a771d578c1ef32957a8c531936048e
Steve Hiehn <stevehiehn@Steves-Mac-mini.hitronhub.home>
2022-06-27 22:20:05 -0700

    wip - progress on loop markers

--- src/components/ComposerControlsLoopBar.vue
+++ src/components/ComposerControlsLoopBar.vue
@@ -1,13 +1,13 @@
 <template>
-  <div ref="loopBar" class="flex w-auto bg-green-500 h-6 hover:cursor-pointer"
+  <div ref="loopBar" class="relative w-auto bg-green-500 h-6 hover:cursor-pointer"
        @click="clickedLoopBar($event)">
-    <div class="w-8 h-6 bg-white">START</div>
-    <div class="w-8 h-6 bg-black text-white">END</div>
+    <div ref="startLoop" class="absolute w-8 h-6 bg-white">START</div>
+    <div ref="endLoop" class="absolute w-8 h-6 bg-black text-white">END</div>
   </div>
 </template>
 
 <script>
-import {inject, ref, watch} from "vue";
+import {inject, onMounted, ref, watch} from "vue";
 import useEventsBus from "../events/eventBus";
 
 export default {
@@ -16,22 +16,64 @@ export default {
     const store = inject('store')
     const {bus} = useEventsBus()
     const loopBar = ref(null)
+    const startLoop = ref(null)
+    const endLoop = ref(null)
 
-    const clickedLoopBar = (event) => {
-      event.preventDefault()
-
-      let horizontalPercentage = Math.round((event.clientX - event.target.getBoundingClientRect().x) / event.target.getBoundingClientRect().width * 100)
+    const getXPosPercentage = (event) => {
+      let clickXPosPercentage = Math.round((event.clientX - event.target.getBoundingClientRect().x) / event.target.getBoundingClientRect().width * 100)
 
       const isInt = (value) => {
         let x = parseFloat(value);
         return !isNaN(value) && (x | 0) === x;
       }
 
-      if (!horizontalPercentage || !isInt(horizontalPercentage)) {
-        horizontalPercentage = 0
+      if (!clickXPosPercentage || !isInt(clickXPosPercentage)) {
+        clickXPosPercentage = 0
       }
 
-      console.log('LOOP_BAR_CLICK_POS', horizontalPercentage)
+      return clickXPosPercentage
+    }
+
+    onMounted(() => {
+      startLoop.value.style.marginLeft = 0 + 'px'
+      endLoop.value.style.marginLeft = 400 + 'px'
+    })
+
+    const getPointBetweenStartAndEnd = () => {
+      const start = parseInt(startLoop.value.style.marginLeft.replace('px', ''))
+      const end = parseInt(endLoop.value.style.marginLeft.replace('px', ''))
+
+      return (end - start) / 2
+    }
+
+    const moveStartOrEndMarker = (event) => {
+      const start = parseInt(startLoop.value.style.marginLeft.replace('px', ''))
+
+      const mousePosX = event.clientX - event.target.getBoundingClientRect().x
+
+      if(mousePosX < (getPointBetweenStartAndEnd() + start)){
+        return 'start'
+      }
+
+      return 'end'
+    }
+
+    const clickedLoopBar = (event) => {
+      event.preventDefault()
+
+      console.log('middle', getPointBetweenStartAndEnd())
+
+      if(moveStartOrEndMarker(event) === 'start') {
+        startLoop.value.style.marginLeft = Math.round((event.clientX - event.target.getBoundingClientRect().x)) + 'px'
+      } else { //move end marker
+        endLoop.value.style.marginLeft = Math.round((event.clientX - event.target.getBoundingClientRect().x)) + 'px'
+      }
+
+
+      //console.log('startLoop.value.style.marginLeft', Math.round((event.clientX - event.target.getBoundingClientRect().x)) + 'px')
+
+      // console.log('LOOP_BAR_WIDTH', event.target.getBoundingClientRect().width)
+      // console.log('LOOP_BAR_CLICK_POS', getXPosPercentage(event))
     }
 
     const convertRemToPixels = (rem) => {
@@ -50,7 +92,7 @@ export default {
       }
     })
 
-    return {clickedLoopBar, loopBar}
+    return {clickedLoopBar, endLoop, loopBar, startLoop}
   }
 }
 </script>
